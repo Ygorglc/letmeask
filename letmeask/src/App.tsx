bbb7543f7a55a9860d80a7354084fefaac089a1c
Ygor <ygorglc@gmail.com>
2021-12-23 12:54:39 -0300

    Erro no react router

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+//import { Room } from './pages/Room';
+//import { AdminRoom } from './pages/AdminRoom';
+
+//import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+          <Route index element={Home} />
+          <Route path="/rooms/new" element={NewRoom} />
+    </BrowserRouter>
   );
 }
 
